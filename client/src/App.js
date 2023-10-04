81b2eb778f3be9357c50352f49b72574a5c1c162
alan-augusto <augustoalan56@gmail.com>
2023-10-04 17:17:22 -0300

    commit incial

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Teste from './teste/teste';
+
+function App() {
+  return (
+    <div className="App">
+      <p> dsadasdas xfsdfsd </p>
+      
+      <Teste
+        titulo='filme 1'
+        endereco='https://th.bing.com/th/id/OIP.G7YmF5RWpGfr5O2PYiXMSQHaK_?pid=ImgDet&rs=1'
+      />
+
+
+    </div>
+  );
+}
+
+export default App;
