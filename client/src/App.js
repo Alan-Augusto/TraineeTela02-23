2a7d63cfadd588113978621b1dfe4d5fd11e870c
Alan Augusto <78389806+Alan-Augusto@users.noreply.github.com>
2023-10-04 17:40:43 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,7 @@ import Teste from './teste/teste';
 function App() {
   return (
     <div className="App">
-      <p> dsadasdas xfsdfsd </p>
-      
-      <Teste
-        titulo='filme 1'
-        endereco='https://th.bing.com/th/id/OIP.G7YmF5RWpGfr5O2PYiXMSQHaK_?pid=ImgDet&rs=1'
-      />
-
-
+      <p> INÍCIO DO PROJETO </p>
     </div>
   );
 }
